31e55bb2f43628b8ad6e535fa88a35959407221d
Marcelo e Thais <thais.kusuki@gmail.com>
2018-03-07 22:04:29 -0300

    create_property

---
+++ spec/features/create_property_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'User create property' do
+  scenario 'successfully' do
+    visit new_property_path
+
+    fill_in 'Título', with: 'Casa de Campo'
+    fill_in 'Descrição', with: 'Uma casa especial para férias.'
+    fill_in 'Localização', with: 'Porto de Galinhas'
+    fill_in 'Bairro', with: 'Vila da Galinha'
+    fill_in 'Foto principal', with: 'foto.jpg'
+    fill_in 'Finalidade', with: 'Férias'
+    fill_in 'Quantidade de cômodos', with: 6
+    check 'Possui acessibilidade'
+    check 'Aceita animais'
+    check 'Aceita fumantes'
+    fill_in 'Máximo de pessoas', with: 12
+    fill_in 'Mínimo de dias para locação', with: 3
+    fill_in 'Máximo de dias para locação', with: 15
+    fill_in 'Preço da diária', with: '250.00'
+    click_on 'Enviar'
+
+    expect(current_path).to eq property_path(1)
+    expect(page).to have_css('h1', text: 'Cadastro de Imóvel')
+    expect(page).to have_css('h1', text: 'Casa de Campo')
+    expect(page).to have_css('h3', text: 'Descrição')
+    expect(page).to have_css('p', text: 'Uma casa especial para férias.')
+    expect(page).to have_css('li', text: 'Localização: Porto de Galinhas')
+    expect(page).to have_css('li', text: 'Bairro: Vila da Galinha')
+    expect(page).to have_css('li', text: 'Foto principal: foto.jpg')
+    expect(page).to have_css('li', text: 'Cômodos: 6')
+    expect(page).to have_css('li', text: 'Finalidade: Férias')
+    expect(page).to have_css('li', text: 'Máximo de pessoas: 12')
+    expect(page).to have_css('li', text: 'Mínimo de dias: 3')
+    expect(page).to have_css('li', text: 'Máximo de dias: 15')
+    expect(page).to have_css('h3', text: 'Condições permitidas:')
+    expect(page).to have_css('li', text: 'Acessibilidade? Sim')
+    expect(page).to have_css('li', text: 'Aceita animais? Sim')
+    expect(page).to have_css('li', text: 'Aceita fumantes? Sim')
+    expect(page).to have_css('li', text: 'Preço da diária: R$ 250')
+    expect(page).to have_content('Voltar')
+  end
+
+  scenario 'fail' do
+  end
+end
--- spec/features/user_view_property_details_spec.rb
+++ spec/features/user_view_property_details_spec.rb
@@ -8,14 +8,15 @@ feature 'User view property details' do
                 property_location: 'Campos de Jordão',
                 neighborhood: 'Campos',
                 rent_purpose: 'Férias',
-                accessibility: true,
-                allow_pets: false,
-                allow_smokers: true,
+                main_photo: 'foto.jpg',
                 rooms: 5,
                 maximum_guests: 15,
                 minimum_rent: 3,
                 maximum_rent: 15,
-                daily_rate: 200.00
+                daily_rate: 200.0,
+                accessibility: true,
+                allow_pets: false,
+                allow_smokers: true,
               )
 
     visit property_path(property)
@@ -26,14 +27,16 @@ feature 'User view property details' do
     expect(page).to have_css('li', text: "Localização: #{property.property_location}")
     expect(page).to have_css('li', text: "Bairro: #{property.neighborhood}")
     expect(page).to have_css('li', text: "Finalidade: #{property.rent_purpose}")
-    expect(page).to have_css('li', text: 'Acessibilidade? Sim')
-    expect(page).to have_css('li', text: 'Aceita animais? Não')
-    expect(page).to have_css('li', text: 'Aceita fumantes? Sim')
+    expect(page).to have_css('li', text: "Foto principal: #{property.main_photo}")
     expect(page).to have_css('li', text: "Cômodos: #{property.rooms}")
     expect(page).to have_css('li', text: "Máximo de pessoas: #{property.maximum_guests}")
     expect(page).to have_css('li', text: "Mínimo de dias: #{property.minimum_rent}")
     expect(page).to have_css('li', text: "Máximo de dias: #{property.maximum_rent}")
     expect(page).to have_css('li', text: "Preço da diária: R$ #{property.daily_rate}")
+    expect(page).to have_css('h3', text: "Condições permitidas:")
+    expect(page).to have_css('li', text: 'Acessibilidade? Sim')
+    expect(page).to have_css('li', text: 'Aceita animais? Não')
+    expect(page).to have_css('li', text: 'Aceita fumantes? Sim')
     expect(page).to have_link('Voltar', href: root_path)
   end
 
