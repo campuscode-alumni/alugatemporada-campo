dd312e7ee54179e681237c0bad05dfedf7e8ae7d
Marcelo e Thais <thais.kusuki@gmail.com>
2018-03-12 22:09:34 -0300

    Add price_range validations

--- app/models/price_range.rb
+++ app/models/price_range.rb
@@ -1,3 +1,13 @@
 class PriceRange < ApplicationRecord
   belongs_to :property
+  validates :description, :start_date, :end_date, :daily_rate,
+      presence: { message: 'É necessário preencher todos os dados da temporada'}
+
+  validate :final_date_is_greater_than_start_date, on: :create
+
+  def final_date_is_greater_than_start_date
+    if start_date && end_date && start_date > end_date
+      errors[:base] << 'A data inicial não pode ser maior que a data final'
+    end
+  end
 end
