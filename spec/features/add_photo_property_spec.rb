1f390519e3048a121020efa4f4912c792a8c079c
Eduardo Koike <eduardo.koike@campuscode.com.br>
2018-03-19 18:22:08 -0300

    [WIP] add country house image

--- spec/features/add_photo_property_spec.rb
+++ spec/features/add_photo_property_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 feature 'owner add a new photo for a property' do
   scenario 'successfuly' do
-    owner = create(:property_owner)
-    login_as(owner, scope: :property_owner)
     property = create(:property)
+    owner = property.property_owner
+
+    login_as(owner, scope: :property_owner)
 
     visit new_property_path(property)
     click_on property_path(property)
