87049fb90c3a0056a231241641a58fb820b468b3
Francisco e Koike <franciscofelixs@gmail.com>
2018-03-20 22:25:39 -0300

    owner reject a proposal

--- spec/features/owner_reject_proposal_spec.rb
+++ spec/features/owner_reject_proposal_spec.rb
@@ -13,13 +13,58 @@ feature 'owner reject a proposal' do
     prop = Proposal.last
 
     expect(prop).to be_rejected
+  end
+
+  scenario 'do not show rejected proposals' do
+    proposal = create(:proposal)
+    owner = proposal.property.property_owner
+
+    login_as(owner, scope: :property_owner)
+
+    visit property_path(proposal.property)
+    click_on 'Rejeitar proposta'
+
+    prop = Proposal.last
+
     expect(current_path).to eq(property_path(proposal.property))
-    expect(page).to_not have_content('Sua proposta foi enviada com sucesso!')
     expect(page).to_not have_css('li', text: 'Finalidade da proposta: Despedida de solteiro')
     expect(page).to_not have_css('li', text: 'Quantidade de hospedes: 12')
-    expect(page).to_not have_css('li', text: "Valor total da proposta: R$2200,00")
     expect(page).to_not have_css('li', text: 'Vou levar meu pet')
     expect(page).to_not have_css('li', text: 'Sou fumante')
     expect(page).to_not have_css('li', text: 'Maiores detalhes: N/A')
+  end
+
+  scenario 'reject only one proposal' do
+    proposal = create(:proposal)
+    user = proposal.user
+    property = proposal.property
+    proposal1 = create(:proposal, property: property, user: user,
+      rent_purpose: 'Festa de arromba',
+      total_guest: 100,
+      details: 'Festa para os jogadores convocados para a NHL',
+      pet: false,
+      smoker: false,
+    )
+    owner = proposal.property.property_owner
+
+    login_as(owner, scope: :property_owner)
+
+    visit property_path(proposal.property)
+    within("div#proposal_#{proposal.id}") do
+      click_on 'Rejeitar proposta'
+    end
+
+    prop = Proposal.last
+
+    expect(current_path).to eq(property_path(proposal.property))
+    expect(page).to_not have_css('li', text: 'Finalidade da proposta: Despedida de solteiro')
+    expect(page).to_not have_css('li', text: 'Quantidade de hospedes: 12')
+    expect(page).to_not have_css('li', text: 'Vou levar meu pet')
+    expect(page).to_not have_css('li', text: 'Sou fumante')
+    expect(page).to_not have_css('li', text: 'Maiores detalhes: N/A')
+
+    expect(page).to have_css('li', text: "Finalidade da proposta: #{prop.rent_purpose}")
+    expect(page).to have_css('li', text: "Quantidade de hospedes: #{prop.total_guest}")
+    expect(page).to have_css('li', text: "Maiores detalhes: #{prop.details}")
   end
 end
